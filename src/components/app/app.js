13b22c75f3a921c0bca1058d6b63018211b7dc0f
andiua <anduhust@ukr.net>
2022-01-30 21:56:17 +0200

    add json db and links

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,17 +2,25 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
-	console.log(RestoService.getMenuItems());
+	RestoService.getMenuItems()
+	.then(menu => console.log(menu))
+	.catch(err => console.log(`Помилка: ${err}`))
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+			<Router>
+				<div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+					<AppHeader total={50}/>
+					<Switch>
+						<Route path='/' exact component={MainPage}/>
+						<Route path='/cart' component={CartPage}/>
+						<Route component={MainPage}/>
+					</Switch>
         </div>
+			</Router>
     )
 }
 
--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,15 @@
 export default class RestoServis {
-	getMenuItems() {
-		return [];
+	getResource = async (url) => {
+		const res = await fetch(`http://localhost:3000${url}`);
+		if (!res.ok){
+			throw new Error(`Could not fetch ${url}` +
+			`, received ${res.status}`);
+		}
+
+		return await res.json();
+	}
+	getMenuItems = async () => {
+		const res = await this.getResource('/menu/')
+		return res;
 	}
 }
